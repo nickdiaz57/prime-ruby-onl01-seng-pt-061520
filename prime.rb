78f8e623e1cd337eaeeb855b291d81a8d5fad601
nickd <nickdiaz58@gmail.com>
2020-06-26 10:56:34 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+    if number <= 1
+        false
+    else
+        i = 2
+        tests = []
+        while i < number do
+            tests.push(number % i)
+            i += 1
+        end
+        if tests.include?(0)
+            false
+        else
+            true
+        end
+    end
+end
+
